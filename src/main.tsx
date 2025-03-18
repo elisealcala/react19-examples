26459e6f6a0c67ca84d05d40e01db7246ab566e2
Elizabeth Alcalá <elizabethalcala28@gmail.com>
2025-03-17 19:38:03 -0500

    feat: add async functions page

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter, Routes, Route } from "react-router";
-import AsyncFunctions from './routes/async-functions.tsx';
+import AsyncFunctionsPage from './routes/async-functions.tsx';
 import Layout from './layout.tsx';
 
 
@@ -12,7 +12,7 @@ createRoot(document.getElementById('root')!).render(
         <Routes>
           <Route element={<Layout />} >
             <Route index element={<App />} />
-            <Route path="async-functions" element={<AsyncFunctions />} />
+            <Route path="async-functions" element={<AsyncFunctionsPage />} />
           </Route>
         </Routes>
     </BrowserRouter>
--- src/routes/async-functions.tsx
+++ src/routes/async-functions.tsx
@@ -1,9 +1,50 @@
+import { useState, useTransition } from "react";
 
+type Data = {
+  title: string;
+  body: string;
+}
+ 
+function AsyncFunctionsPage() {
+  const [data, setData] = useState<Data | null>(null);
+  const [isPending, startTransition] = useTransition();
 
-const AsyncFunctions = () => {
+  const fetchData = async () => {
+    startTransition(async () => {
+      const response = await fetch("https://jsonplaceholder.typicode.com/posts/2");
+      const result = await response.json();
+      setData(result);
+    });
+  };
+
   return (
-    <div>async functions</div>
-  )
+    <div>
+      <h2 className="font-bold text-3xl mb-5">Async Functions in Transitions</h2>
+
+      <div>
+        <p className="mb-5">
+          This example demonstrates how to use async functions in transitions.
+        </p>
+      </div>
+      <div className="flex items-center space-x-5 mb-5">
+        <button onClick={fetchData} disabled={isPending}>
+          {isPending ? "Loading..." : "Fetch Data"}
+        </button>
+        <button onClick={() => setData(null)} disabled={isPending}>
+          Clear Data
+        </button>
+      </div>
+      <div className="flex flex-col space-y-10">
+        {data && (
+          <div>
+            <h3>{data.title}</h3>
+            <p>{data.body}</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default AsyncFunctions
+
+export default AsyncFunctionsPage
