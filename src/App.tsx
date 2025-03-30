306ac1a816ed537023ebe9f50d981c5e53184dfe
Elizabeth Alcalá <elizabethalcala28@gmail.com>
2025-03-30 11:58:52 -0500

    feat: update index:

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ function App() {
 
   return (
     <>
-      <div>
-        sadas
-      </div>
+      <h1>
+        <a className="text-xl font-bold mb-5 black" target="_blank" href="https://medium.com/@elisealcala/react-19-everything-you-need-to-know-ff65674c833c">React 19: Everything You Need to Know</a> 
+      </h1>
     </>
   )
 }
